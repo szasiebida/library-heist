5f82dd94f4f3a99bc4425057596362825c31f6f6
liyas1719 <lasariaissa@smith.edu>
2024-11-21 22:42:57 -0500

    began user input

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 // Ive only just started this class because all i know is that it needs to have a map of all the rooms!
 
@@ -19,6 +20,47 @@ public class Game {
     }   
 
     public static void main(String[] args) {
-        
+         //This is a "flag" to let us know when the loop should end
+        boolean stillPlaying = true;
+
+        // We'll use this to get input from the user.
+        Scanner userInput = new Scanner(System.in);
+
+        // Storage for user's responses
+        String userResponse = "";
+
+        //OPENEING
+        System.out.println("Welcome to Smith College, home of the rare books collection. You have been hired to steal the entire collection before dawn. If completed, you will be paid $1,000,000.00 in cash. If not, you will face the consequences of getting caught. Good luck.");
+    
+        //INSTRUCITONS
+        System.out.println("You are currently located.... To the North is.... To the South is.... To the East is.... To the West is.... Your inventory is currently empty.");        
+    
+        // The do...while structure means we execute the body of the loop once before checking the stopping condition
+        //do {
+//             // ************************************************
+//             // The stuff that happens in your game will go here
+            userResponse = userInput.nextLine().toUpperCase();
+
+            // ***********************************************************************
+            // And as the player interacts, you'll check to see if the game should end
+            //  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓
+            //if (userResponse.equals("WIN") || userResponse.equals("LOSE")) {
+                //stillPlaying = false;
+            //}
+        //} while (stillPlaying);
+
+        // Tidy up
+        //userInput.close();
+
+        // Once you exit the loop, you may need to deal with various possible stopping conditions
+        // if (userResponse.equals("WIN")) {
+        //     System.out.println("Yay, you won!");
+        // } else { // userResponse.equals("LOSE")
+        //     System.out.println("Better luck next time.");
+        // }
+
+   // }
     }
 }
+
+
