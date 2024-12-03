606402198583cafc0dd8fd0e368e9b1c485b38aa
sofia <173196545+szasiebida@users.noreply.github.com>
2024-12-03 11:40:20 -0500

    item class

--- user.java
+++ user.java
@@ -1,23 +1,13 @@
 import java.util.ArrayList; // import the ArrayList class
 public class User {
 
-    private String initialInventory; //WHAT IF WE TAKE IN A STRING AND THEN SEGMENT IT AND THEN WE CAN MAKE IT AN ARRAYLIST
+    // private String initialInventory; //WHAT IF WE TAKE IN A STRING AND THEN SEGMENT IT AND THEN WE CAN MAKE IT AN ARRAYLIST
     int location; //index of the arraylist that corresponds to the room 
 
-    public User(String initialInventory, int location) {
-        this.initialInventory = initialInventory;
+    public User( int location) {
+        // this.initialInventory = initialInventory;
         this.location=location;
         // append the existing inventory to an arraylist and then the arraylist becomes the offical inventory
-    }
-
-    public String useitem() {
-        //remove item from inventory
-        return this.initialInventory;
-    }
-                // HEY WHATS THE DIFF BETWEEN THESE TWO
-    public String grabitem() {
-        //add to inventory
-        return this.initialInventory;
     }
 
     public void move(String command,ArrayList<Room> map){
