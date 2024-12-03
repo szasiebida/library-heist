8da7e9ac6b422505ef92ba4c232d45c61a2b32cb
liyas1719 <lasariaissa@smith.edu>
2024-12-03 11:46:51 -0500

    added move function

--- Game.java
+++ Game.java
@@ -30,46 +30,56 @@ public class Game {
         Game myGame= new Game();
         myGame.play();
 
-//          //This is a "flag" to let us know when the loop should end
-//         boolean stillPlaying = true;
+         //This is a "flag" to let us know when the loop should end
+        boolean stillPlaying = true;
 
-//         // We'll use this to get input from the user.
-//         Scanner userInput = new Scanner(System.in);
+        // We'll use this to get input from the user.
+        Scanner userInput = new Scanner(System.in);
 
-//         // Storage for user's responses
-//         String userResponse = "";
+        // Storage for user's responses
+        String userResponse = "";
 
-//         //OPENEING
-//         System.out.println("Welcome to Smith College, home of the rare books collection. You have been hired to steal the entire collection before dawn. If completed, you will be paid $1,000,000.00 in cash. If not, you will face the consequences of getting caught. Good luck.");
+        //OPENEING
+        System.out.println("Welcome to Smith College, home of the rare books collection. You have been hired to steal the entire collection before dawn. If completed, you will be paid $1,000,000.00 in cash. If not, you will face the consequences of getting caught. Good luck.");
     
-//         //INSTRUCITONS
-//         System.out.println("You are currently located.... To the North is.... To the South is.... To the East is.... To the West is.... Your inventory is currently empty.");        
+        //INSTRUCITONS
+        System.out.println("You are currently located.... To the North is.... To the South is.... To the East is.... To the West is.... Your inventory is currently empty.");        
     
-//         // The do...while structure means we execute the body of the loop once before checking the stopping condition
-//         //do {
-// //             // ************************************************
-// //             // The stuff that happens in your game will go here
-//             userResponse = userInput.nextLine().toUpperCase();
+        // The do...while structure means we execute the body of the loop once before checking the stopping condition
+        do {
 
-//             // ***********************************************************************
-//             // And as the player interacts, you'll check to see if the game should end
-//             //  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓
-//             //if (userResponse.equals("WIN") || userResponse.equals("LOSE")) {
-//                 //stillPlaying = false;
-//             //}
-//         //} while (stillPlaying);
+            userResponse = userInput.nextLine().toLowerCase();
 
-//         // Tidy up
-//         //userInput.close();
+            if (userResponse.equals("n")) {
+                myGame.myUser.move("n", myGame.map);
+            } else if (userResponse.equals("s")) {
+                myGame.myUser.move("s", myGame.map);
+            } else if (userResponse.equals("e")) {
+                myGame.myUser.move("e", myGame.map);
+            } else if (userResponse.equals("w")) {
+                myGame.myUser.move("w", myGame.map);
+            } else if (userResponse.contains("use")) {
+                //calluseitem
+            } else if (userResponse.contains("grab")||userResponse.contains("take")) {
+                //callgrabitem
+            } else {
+                System.out.println("We don't have that function. Please try something else.");
+            }
 
-//         // Once you exit the loop, you may need to deal with various possible stopping conditions
-//         // if (userResponse.equals("WIN")) {
-//         //     System.out.println("Yay, you won!");
-//         // } else { // userResponse.equals("LOSE")
-//         //     System.out.println("Better luck next time.");
-//         // }
 
-//    // }
+        } while (stillPlaying);
+
+        // Tidy up
+        userInput.close();
+
+        // Once you exit the loop, you may need to deal with various possible stopping conditions
+        // if (userResponse.equals("WIN")) {
+        //     System.out.println("Yay, you won!");
+        // } else { // userResponse.equals("LOSE")
+        //     System.out.println("Better luck next time.");
+        // }
+
+   // }
     }
 }
 
