f7b9a53b9f78d05b36854bc86424fe6e29329426
sofia <173196545+szasiebida@users.noreply.github.com>
2024-12-12 14:46:30 -0500

    text edits

--- Game.java
+++ Game.java
@@ -299,13 +299,13 @@ public class Game {
         System.out.println("**********************");
 
         System.out.println("\n");
-        System.out.println("Welcome to Smith College, home of the rare books collection. You have been hired to steal the entire collection before dawn. If completed, you will be paid       $1,000,000.00 in cash. If not, you will face the consequences of getting caught. Good luck.");
+        System.out.println("Welcome to Smith College, home of the rare books collection. You have been hired to steal the entire collection before dawn. If completed, you will be paid $1,000,000.00 in cash. If not, you will face the consequences of getting caught. Good luck.");
 
         //INSTRUCITONS
         System.out.println("\n");
         System.out.println("\033[3m"+"You are currently located in Bass Hall. To the North is Neilson Library. To the East is nothing. To the South is nothing. To the West is Burton Hall. "+"\033[0m");
         System.out.println("\n"); 
-        System.out.println("Your inventory is currently empty. Use n, s, e, w to move. You can access your inventory at any time by inputting inventory.");
+        System.out.println("Your inventory is currently empty. You can access your inventory at any time by inputting inventory. You can drop items at any time by typing drop ____. Use n, s, e, w to move. You can move anywhere at any time. Respond to the game promts with yes/no or 1/2 depending on the prompt.");
         System.out.println("\n");        
         myGame.buildingscripts("yes");
         // The do...while structure means we execute the body of the loop once before checking the stopping condition
